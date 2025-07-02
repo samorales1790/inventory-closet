be1713f1da26b77f9699327ac3627f4dec40364d
samorales1790 <samorales@onemedical.com>
2025-07-02 11:41:07 -0400

    Update inventory.js

--- inventory.js
+++ inventory.js
@@ -1,9 +1,12 @@
 const inventory = [
   { name: "Blood Tubes (LC/ Quest tubes)", drawer: "D1", tower: "Tower 1", category: "Lab Supplies" },
+  { name: "Tourniquets", drawer: "D1", tower: "Tower 1", category: "Lab Supplies" },
   { name: "Urine Aptimas", drawer: "D1", tower: "Tower 2", category: "Urine" },
   { name: "Pregnancy Test Kits", drawer: "D2", tower: "Tower 2", category: "Urine" },
-  { name: "Pap Brooms/ Brushes", drawer: "D3", tower: "Tower 4", category: "OB/GYN Supplies" },
-  { name: "Suture kits & Skin Closure Strips", drawer: "D4", tower: "Tower 5", category: "Procedure/ Injectables" },
-  { name: "Alcohol Pads", drawer: "D2", tower: "Tower 6", category: "Exam Rooms" },
-  { name: "Medium Gloves", drawer: "D3", tower: "Tower 7", category: "Bathroom/ Kitchen" }
+  { name: "Stool Kits", drawer: "D1", tower: "Tower 3", category: "Misc Lab" },
+  { name: "Unisex Aptimas", drawer: "D1", tower: "Tower 4", category: "OB/GYN Supplies" },
+  { name: "EKG/ Ear Lavage Items", drawer: "D1", tower: "Tower 5", category: "Procedure/ Injectables" },
+  { name: "Disinfectants: Betadine & Isopropyl Alcohol", drawer: "D1", tower: "Tower 6", category: "Exam Rooms" },
+  { name: "Procedure Face Masks", drawer: "D1", tower: "Tower 7", category: "Bathroom/ Kitchen" },
+  { name: "Hand Lotion", drawer: "D1", tower: "Tower 8", category: "Miscellaneous" }
 ];
